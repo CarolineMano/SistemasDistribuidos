22bb87e6ae1e090ad78306eb71a0279baae8e88b
CarolineMano <carolinemano@protonmail.ch>
2022-05-25 21:32:57 -0300

    Added Usuario and Perfil

--- src/main/java/uam/controllers/ProdutoController.java
+++ src/main/java/uam/controllers/ProdutoController.java
@@ -3,7 +3,7 @@ package uam.controllers;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.http.HttpStatus;
+//import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
