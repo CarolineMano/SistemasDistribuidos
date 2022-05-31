98f10174b2e7912a0b7cde1d46caaf2c3cd9a864
G1ss <Giovanio9@outlook.com>
2022-05-30 21:07:34 -0300

    isert table estoque

--- src/main/resources/db/migration/V03__criacao_tabela_estoque.sql
+++ src/main/resources/db/migration/V03__criacao_tabela_estoque.sql
@@ -6,3 +6,8 @@ valor DECIMAL,
 id_usuarios  INT,
 FOREIGN KEY (id_usuarios) REFERENCES usuarios (id)
 );
+
+
+INSERT INTO estoque (nome, quantidade, valor, id_usuarios) VALUES ('nutella', 6, 12.50, 1);
+INSERT INTO estoque (nome, quantidade, valor, id_usuarios) VALUES ('ruffles', 15, 8.20, 1);
+INSERT INTO estoque (nome, quantidade, valor, id_usuarios) VALUES ('budwiser', 30, 2.29, 1);
