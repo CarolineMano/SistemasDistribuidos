6bb6342c47e8433871702e3cbed264d8b7f34596
CarolineMano <carolinemano@protonmail.ch>
2022-05-30 21:47:58 -0300

    Fixed some bugs

--- src/main/resources/db/migration/V03__criacao_tabela_estoque.sql
+++ src/main/resources/db/migration/V03__criacao_tabela_estoque.sql
@@ -3,11 +3,11 @@ id INT PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR(80) NOT NULL,
 quantidade INT,
 valor DECIMAL,
-id_usuarios  INT,
-FOREIGN KEY (id_usuarios) REFERENCES usuarios (id)
+id_usuario  INT,
+FOREIGN KEY (id_usuario) REFERENCES usuarios (id)
 );
 
 
-INSERT INTO estoque (nome, quantidade, valor, id_usuarios) VALUES ('nutella', 6, 12.50, 1);
-INSERT INTO estoque (nome, quantidade, valor, id_usuarios) VALUES ('ruffles', 15, 8.20, 1);
-INSERT INTO estoque (nome, quantidade, valor, id_usuarios) VALUES ('budwiser', 30, 2.29, 1);
+INSERT INTO estoque (nome, quantidade, valor, id_usuario) VALUES ('nutella', 6, 12.50, 1);
+INSERT INTO estoque (nome, quantidade, valor, id_usuario) VALUES ('ruffles', 15, 8.20, 1);
+INSERT INTO estoque (nome, quantidade, valor, id_usuario) VALUES ('budwiser', 30, 2.29, 1);
